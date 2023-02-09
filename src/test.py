0fddb4ca3227fb22325f84f414d588d80855760c
Aditya Rathi <adityamr@andrew.cmu.edu>
2023-02-09 16:02:09 -0500

    Update test.py
    Start writing code to run lcc net on our data

--- src/test.py
+++ src/test.py
@@ -21,8 +21,8 @@ from skimage import io
 from tqdm import tqdm
 import time
 
-from models.LCCNet import LCCNet
-from DatasetLidarCamera import DatasetLidarCameraKittiOdometry
+
+from LCCNet.models.LCCNet import LCCNet
 
 from quaternion_distances import quaternion_distance
 from utils import (mat2xyzrpy, merge_inputs, overlay_imgs, quat2mat,
@@ -32,10 +32,18 @@ from utils import (mat2xyzrpy, merge_inputs, overlay_imgs, quat2mat,
 from torch.utils.data import Dataset
 from pykitti import odometry
 import pandas as pd
-from PIL import Image
+from PIL import Image as im
 from math import radians
 from utils import invert_pose
 from torchvision import transforms
+
+import rospy
+import tf2_ros
+import pcl_ros
+from sensor_msgs.msg import Image, PointCloud2
+import ros_numpy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_2D_lidar_projection(pcl, cam_intrinsic):
     pcl_xyz = cam_intrinsic @ pcl.T
@@ -49,7 +57,7 @@ def get_2D_lidar_projection(pcl, cam_intrinsic):
 
 def lidar_project_depth(pc_rotated, cam_calib, img_shape):
     pc_rotated = pc_rotated[:3, :].detach().cpu().numpy()
-    cam_intrinsic = cam_calib.numpy()
+    cam_intrinsic = cam_calib
     pcl_uv, pcl_z = get_2D_lidar_projection(pc_rotated.T, cam_intrinsic)
     mask = (pcl_uv[:, 0] > 0) & (pcl_uv[:, 0] < img_shape[1]) & (pcl_uv[:, 1] > 0) & (
             pcl_uv[:, 1] < img_shape[0]) & (pcl_z > 0)
@@ -66,4 +74,57 @@ def lidar_project_depth(pc_rotated, cam_calib, img_shape):
 
     return depth_img, pcl_uv, pc_valid
 
+class lidar_cam:
+    def __init__(self):
+        tfBuffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(tfBuffer)
+        self.sub1 = rospy.Subscriber("/theia/left_camera/color/image_raw",Image,self.cam_callback)
+        self.sub2 = rospy.Subscriber("/theia/os_cloud_node/points",PointCloud2,self.lidar_callback)
+        self.sub3 = rospy.Subscriber
+        self.image = np.array()
+        self.pcl_arr = np.array()
+        self.cam_intrinsic = np.array([[615.3355712890625, 0.0, 333.37738037109375], [0.0, 615.457763671875, 233.50408935546875], [0.0, 0.0, 1.0]])
+        self.main()
+    
+    def cam_callback(self,data):
+        data = ros_numpy.numpify(data)
+        self.image = im.fromarray(data)
+ 
+    def lidar_callback(self,data):
+        self.pcl_arr = ros_numpy.numpify(data)
+    
+    def main(self):
+        input_size = (256, 512)
+    
+        weights = [
+            'home/cerlab/submodule_ws/src/lidar_camera_fusion/include/LCCNet/pretrained/kitti_iter1.tar',
+            'home/cerlab/submodule_ws/src/lidar_camera_fusion/include/LCCNet/pretrained/kitti_iter2.tar',
+            'home/cerlab/submodule_ws/src/lidar_camera_fusion/include/LCCNet/pretrained/kitti_iter3.tar',
+            'home/cerlab/submodule_ws/src/lidar_camera_fusion/include/LCCNet/pretrained/kitti_iter4.tar',
+            'home/cerlab/submodule_ws/src/lidar_camera_fusion/include/LCCNet/pretrained/kitti_iter5.tar'
+        ]
+
+        models = []
+
+        for i in range(len(weights)):
+            model = LCCNet(input_size, use_feat_from=-1, md=4,
+                                use_reflectance=False, dropout=0.0)
+            checkpoint = torch.load(weights[i], map_location='cpu')
+            saved_state_dict = checkpoint['state_dict']
+            model.load_state_dict(saved_state_dict)
+            model = model.to(device)
+            model.eval()
+            models.append(model)
+
+        rgb_resize = 
+        
+        for iteration in range(len(weights)):
+            T_predicted, R_predicted = models[iteration](rgb_resize, lidar_resize)
+
+
+
+
+
 def main():
+    rospy.init_node('cam_lidar_transform')
+    
